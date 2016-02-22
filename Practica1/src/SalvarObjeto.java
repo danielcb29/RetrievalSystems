52bbecf8c56020c5a16f2083a6ec158e2bc3268b
Daniel Correa <danielc.web@gmail.com>
2016-02-22 19:54:14 +0100

    cambios en el crawler

--- Practica1/src/SalvarObjeto.java
+++ Practica1/src/SalvarObjeto.java
@@ -4,19 +4,12 @@ import java.io.*;
 import java.util.*;
 
 public class SalvarObjeto {
-        public static void main (String args[]) {
-                Hashtable h = new Hashtable();
-                h.put("String","Luis Rodr�guez Dur�n");
-                h.put("Integer",new Integer(2));
-                h.put("Double",new Double(0.96));
-                try {
-                        FileOutputStream fos = new FileOutputStream("h.ser");
-                        ObjectOutputStream oos = new ObjectOutputStream(fos);
-                        oos.writeObject(h);
-                }
-                catch (Exception e) { System.out.println(e); }
-        }
-        public String guardarMap(){
-        	return null;
+        public void guardarMap(Map vocabulario){
+        	try {
+                FileOutputStream fos = new FileOutputStream("dicc.ser");
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(vocabulario);
+	        }
+	        catch (Exception e) { System.out.println(e); }
         }
 }
